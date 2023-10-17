0b8333b18da3e22369c74d2a238fdc61d5b4cba4
lLe Minh Hao <leminhhao2014@yahoo.com>
2023-10-17 17:07:58 +0700

    Remove dependecies and start from scratch

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,30 +1,8 @@
-'use client';
-
-import { SimpleGrid } from "@chakra-ui/react";
-import Header from "./components/header";
-import Product from "./components/product";
-
 
 export default function Home() {
   return (
     <main>
-      <Header/>
-
-      <SimpleGrid
-        padding="10px"
-        columns={{
-          base: 1,
-          md: 3,
-          lg: 6,
-        }}
-        spacing='20px'
-      >
-        <Product />
-        <Product />
-        <Product />
-        <Product />
-        <Product />
-      </SimpleGrid>
+      Main page
     </main>
   )
 }
