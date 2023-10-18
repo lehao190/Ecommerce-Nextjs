210daa1139a3f6bf04a5bbb2025a970c182b4649
lLe Minh Hao <leminhhao2014@yahoo.com>
2023-10-18 14:36:41 +0700

    clear code and migrate to shadcn/ui tailwind

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,11 @@
+import { Button } from "@/components/ui/button"
 
 export default function Home() {
   return (
     <main>
       Main page
+      <h1 className="text-3xl font-bold underline bg-red-50">Hello, Next.js!</h1>
+      <Button>Click me</Button>
     </main>
   )
 }
